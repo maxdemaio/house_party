d1e6aca33e25d8d27d41cb9ba26fecf6b059b40b
Maxwell DeMaio <maxdman321@gmail.com>
2021-01-26 23:33:43 -0500

    Setup serializer and view for rest_api

---
+++ music_controller/api/serializers.py
@@ -0,0 +1,13 @@
+"""
+Take our Room model and translate
+into a JSON response for our React frontend
+"""
+
+from rest_framework import serializers
+from .models import Room
+
+class RoomSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Room
+        fields = ('id', 'code', 'host',
+            'guest_can_pause', 'votes_to_skip')
