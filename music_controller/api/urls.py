1c1441b32d69fb2fd9dfa8528472d44bcce6111c
Maxwell DeMaio <maxdman321@gmail.com>
2021-02-15 23:30:18 -0500

    Added api route for joining rooms

--- music_controller/api/urls.py
+++ music_controller/api/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
-from .views import RoomView, CreateRoom
+from .views import RoomView, CreateRoom, GetRoom, JoinRoom
 
 urlpatterns = [
     path('room', RoomView.as_view()),
-    path('create-room', CreateRoom.as_view())
+    path('create-room', CreateRoom.as_view()),
+    path('get-room', GetRoom.as_view()),
+    path('join-room', JoinRoom.as_view())
 ]
--- music_controller/api/views.py
+++ music_controller/api/views.py
@@ -5,13 +5,60 @@ from .models import Room
 from rest_framework.views import APIView
 from rest_framework.response import Response
 
-# Create your views here.
 class RoomView(generics.ListAPIView):
     """ Return all rooms """
     queryset = Room.objects.all()
     serializer_class = RoomSerializer
 
+
+class GetRoom(APIView):
+    """ Return requested room """
+    serializer_class = RoomSerializer
+    lookup_url_kwarg = 'code'
+
+    def get(self, request, format=None):
+        # Grab code from frontend request '?code=...'
+        code = request.GET.get(self.lookup_url_kwarg)
+
+        # Make sure we have a room code
+        if code != None:
+            # Get room queryset from database
+            room = Room.objects.filter(code=code)
+
+            # Make sure code is valid
+            if len(room) > 0:
+                # Serialize room and take data (python dict)
+                data = RoomSerializer(room[0]).data
+                # Set boolean in data if user is the host
+                data['is_host'] = self.request.session.session_key == room[0].host
+                return Response(data, status=status.HTTP_200_OK)
+            return Response({'Room Not Found': 'Invalid Room Code.'}, status=status.HTTP_404_NOT_FOUND)
+        return Response({'Bad Request': 'Code parameter not found in request'}, status=status.HTTP_404_NOT_FOUND)
+
+
+class JoinRoom(APIView):
+    lookup_url_kwarg = 'code'
+
+    """ Have a user to join a room """
+    def post(self, request, format=None):
+        # Create session
+        if not self.request.session.exists(self.request.session.session_key):
+            self.request.session.create()
+
+        code = request.data.get(self.lookup_url_kwarg)
+        if code != None:
+            room = Room.objects.filter(code=code)
+            if len(room) > 0:
+                # This user's session is in this room
+                # We store the room code they're in server side
+                self.request.session['room_code'] = code
+                return Response({'message': 'Room Joined!'}, status.HTTP_200_OK)
+            return Response({'Bad Request': 'Invalid room code'}, status.HTTP_404_NOT_FOUND)
+        return Response({'Bad Request': 'Invalid post data, did not find code key'}, status=HTTP_404_NOT_FOUND)
+
+
 class CreateRoom(APIView):
+    """ Create Room """
     serializer_class = CreateRoomSerializer
 
     def post(self, request, format=None):
@@ -36,9 +83,13 @@ class CreateRoom(APIView):
                 room.guest_can_pause = guest_can_pause
                 room.votes_to_skip = votes_to_skip
                 room.save(update_fields=['guest_can_pause', 'votes_to_skip'])
+                # We store the room code they're the host of server side
+                self.request.session['room_code'] = room.code
             else:
                 # No previous rooms made by host, make new row in DB
                 room = Room(host=host, guest_can_pause=guest_can_pause, votes_to_skip=votes_to_skip)
                 room.save()
+                # We store the room code they're the host of server side
+                self.request.session['room_code'] = room.code
             
             return Response(RoomSerializer(room).data, status=status.HTTP_200_OK)
